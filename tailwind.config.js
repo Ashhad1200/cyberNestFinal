3954d35d106fd2219e0be895d2a04cb98a8a80aa
Syed Ashhad <syedashad17@gmail.com>
2024-09-16 17:47:00 +0500

    theme fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
 
+// tailwind.config.js
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#1D4ED8',  // Example custom primary color
+        secondary: '#9333EA',  // Example custom secondary color
+      },
       fontFamily: {
-        sans: ['"Inter"'],
+        sans: ['Inter', 'sans-serif'],  // Example custom font family
       },
     },
   },
   plugins: [],
-};
+}
+
