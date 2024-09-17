74e326adfad1c8c5fa52ca48c0bf0ae24b75e4ce
Syed Ashhad <syedashad17@gmail.com>
2024-09-17 10:13:17 +0500

    theme fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@
 // tailwind.config.js
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode : "class" ,
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
         primary: '#1D4ED8',  // Example custom primary color
         secondary: '#9333EA',  // Example custom secondary color
